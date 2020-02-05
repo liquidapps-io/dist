c9f32557db710e8cbdc0fcc8f6ee1e8c03b0b9bc
Tal Muskal <tal@liquidapps.io>
2020-02-05 11:28:35 +0000

    sync

--- contracts/eos/dappservices/ipfs.hpp
+++ contracts/eos/dappservices/ipfs.hpp
@@ -34,7 +34,7 @@ SVC_ACTION(cleanup, false, ((std::string)(uri)),     \
     _ipfs_cleanup(size, uri, current_provider); \
     SEND_SVC_SIGNAL(cleanup, current_provider, package, size, uri)                         \
 }; \
-SVC_ACTION(warmup, true, ((std::string)(uri))((eosio::name)(code)),     \
+SVC_ACTION(warmup, true, ((std::string)(uri)),     \
          ((uint32_t)(size))((std::string)(uri)), \
          ((uint32_t)(size))((std::string)(uri))((std::vector<char>)(data)),TONAME(SVC_CONTRACT_NAME_IPFS) ) \
 { \
@@ -47,8 +47,8 @@ static void svc_ipfs_commit(std::vector<char> data) { \
 static void svc_ipfs_cleanup(std::string uri) { \
     SEND_SVC_REQUEST(cleanup, uri) \
 };\
-static void svc_ipfs_warmup(std::string uri, eosio::name code) { \
-    SEND_SVC_REQUEST(warmup, uri, code) \
+static void svc_ipfs_warmup(std::string uri) { \
+    SEND_SVC_REQUEST(warmup, uri) \
 };
 
 
